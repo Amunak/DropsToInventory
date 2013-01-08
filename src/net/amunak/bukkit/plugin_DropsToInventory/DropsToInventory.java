d16c19ed60ba2c7ac9c47d0d3312cb4c26e6e2fc
Amunak <>
2013-01-08 12:06:05 +0100

    Added configuration

--- src/net/amunak/bukkit/plugin_DropsToInventory/DropsToInventory.java
+++ src/net/amunak/bukkit/plugin_DropsToInventory/DropsToInventory.java
@@ -4,16 +4,21 @@ package net.amunak.bukkit.plugin_DropsToInventory;
  *
  * @author Amunak
  */
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
  
 public final class DropsToInventory extends JavaPlugin {
     
     protected static Log log;
+    protected FileConfiguration config;
  
     @Override
     public void onEnable(){
         log = new Log(this);
         log.info("Plugin enabled");
+        
+        this.saveDefaultConfig();
+        config = this.getConfig();
     }
  
     @Override
