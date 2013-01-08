c3682f5f6d4845f77ab74be30906da05270e61f7
Amunak <git@amunak.net>
2013-01-08 20:18:01 +0100

    Removed final keyword it is not needed

--- src/net/amunak/bukkit/plugin_DropsToInventory/DropsToInventory.java
+++ src/net/amunak/bukkit/plugin_DropsToInventory/DropsToInventory.java
@@ -18,7 +18,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DropsToInventory extends JavaPlugin implements Listener {
+public class DropsToInventory extends JavaPlugin implements Listener {
 
     protected static Log log;
     protected FileConfiguration config;
