e5b6f0ade8a1c398bdbd1a7c5d055fbb49cf00ad
Amunak <git@amunak.net>
2013-01-11 00:20:31 +0100

    moved onBlockBreakEvent to its own class

---
+++ src/net/amunak/bukkit/plugin_DropsToInventory/BlockBreakEventListener.java
@@ -0,0 +1,82 @@
+package net.amunak.bukkit.plugin_DropsToInventory;
+
+/**
+ * Copyright 2013 Jiří Barouš
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.util.Collection;
+import java.util.List;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+/**
+ *
+ * @author Amunak
+ */
+public final class BlockBreakEventListener implements Listener {
+    public List<String> blockFilter;
+    public List<String> safeBlocks;
+
+    public BlockBreakEventListener(DropsToInventory plugin) {
+        blockFilter = plugin.config.getStringList("lists.blockFilter");
+        safeBlocks = plugin.config.getStringList("lists.safeBlocks");
+        Commons.fixEnumLists(blockFilter, safeBlocks);
+    }
+
+    public void onBlockBreakEvent(BlockBreakEvent event, DropsToInventory plugin) {
+        if (!plugin.config.getBoolean("options.general.useOnlySafeBlocks") || safeBlocks.contains(event.getBlock().getType().toString())) {
+            if (plugin.config.getBoolean("options.blocks.ignoreEnchantmentBug") || !enchantBugPresent(event)) {
+                if (plugin.config.get("options.blocks.filterMode").equals("blacklist")) {
+                    if (!blockFilter.contains(event.getBlock().getType().toString())) {
+                        this.moveBlockDropToInventory(event, plugin);
+                    }
+                } else if (plugin.config.get("options.blocks.filterMode").equals("whitelist")) {
+                    if (blockFilter.contains(event.getBlock().getType().toString())) {
+                        this.moveBlockDropToInventory(event, plugin);
+                    }
+                } else {
+                    this.moveBlockDropToInventory(event, plugin);
+                }
+            }
+        }
+    }
+
+    private boolean enchantBugPresent(BlockBreakEvent event) {
+        Collection<Enchantment> buggedEnchants = null;
+        buggedEnchants.add(Enchantment.LOOT_BONUS_BLOCKS);
+        buggedEnchants.add(Enchantment.SILK_TOUCH);
+        for (Enchantment enchantment : buggedEnchants) {
+            if (event.getPlayer().getInventory().getItemInHand().getEnchantmentLevel(enchantment) > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * extracts drop info from block break event and passes it further, managing
+     * block break necessities
+     *
+     * @param event block break event
+     */
+    private void moveBlockDropToInventory(BlockBreakEvent event, DropsToInventory plugin) {
+        plugin.moveDropToInventory(event.getPlayer(), event.getBlock().getDrops(event.getPlayer().getItemInHand()), event.getExpToDrop(), event.getBlock().getLocation());
+        event.setCancelled(true);
+        event.getBlock().setTypeId(Material.AIR.getId());
+    }
+
+}
